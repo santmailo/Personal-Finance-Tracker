04027346a98a00ee99d9574ca014849aa0fa209e
santmailo <iamsunil006@gmail.com>
2024-01-21 21:49:55 +0530

    project setup

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,7 @@
+// import React from "react";
+import "./styles.css";
+function Header() {
+  return <div className="navbar">Header</div>;
+}
+
+export default Header;
---
+++ src/components/SignupSignin/index.jsx
@@ -0,0 +1,8 @@
+// import React from "react";
+import "./styles.css";
+
+function SignUpSignIn() {
+  return <div>SignUpSignIn</div>;
+}
+
+export default SignUpSignIn;
